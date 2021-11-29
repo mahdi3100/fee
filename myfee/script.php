2a97ecc36eb4c371dba2e744d31d4f1abea0da4f
Mehdi <78046337+mahdi3100@users.noreply.github.com>
2021-11-29 17:56:44 +0100

    Delete myfee directory

--- myfee/script.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-
-require 'vendor/autoload.php';
-use Candidat\Fee\Service\NewCSV;
-$getcsvpath = $argv[1];
-$NewCSV = new NewCSV($getcsvpath);
-$NewCSV->checkFormatCSV();
-$NewCSV->FeeResault();
